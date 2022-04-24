ec34e7882f704f3275e7356b24e944b6e3571c6f
thiago <thiagosoares111@yahoo.com.br>
2022-04-24 16:51:45 -0300

    exercicios do useMemo
    O useMemo e bem simples, ele guarda uma ação que pode demorar para ser finalizada e só a chama quando algo nas dependências for mudado, assim não prejudicando o funcionamento da pagina como um todo.

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseMemo = (props) => {
+    const [n1,setN1] = useState(0)
+    const [n2,setN2] = useState(0)
+    const [n3,setN3] = useState(0)
+    function lenta(n1,n2){
+        const future = Date.now() + 2000
+        while (Date.now() < future) { }
+        return n1+n2 
+    }
+
+    const result1 = useMemo(()=>lenta(n1,n2),[n1,n2])
     return (
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
                 subtitle="Retorna um valor memoizado!"
             />
+            <input type="number" className="input" 
+            value={n1} onChange={e=>setN1(parseInt(e.target.value))}/>
+            <input type="number" className="input" 
+            value={n2} onChange={e=>setN2(parseInt(e.target.value))}/>
+            <input type="number" className="input" 
+            value={n3} onChange={e=>setN3(e.target.value)}/>
+            <span className="text">{result1}</span>
         </div>
     )
 }
