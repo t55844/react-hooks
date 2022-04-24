76db717af727c07054b4bd6ac18d15057aee846e
thiago <thiagosoares111@yahoo.com.br>
2022-04-24 15:29:05 -0300

    exercicios do useReff
    Resumindo basicamente você tem uma referencia a um valor setado na criação do useReff que quando alterado não renderizar a tela.
    O valor passado pelo .current do useReff é uma referencia ao objeto original e não uma copia, o que faz com que se altere o valor verdadeiro e não uma copia

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseRef = (props) => {
+    const [value1,setValue1] = useState('')
+    const count = useRef(0)
+    useEffect(()=>{
+        count.current++
+    },[value1])
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <div className="center">
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{value1}[</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <input type="text" className="input" 
+                value={value1} onChange={e=>{setValue1(e.target.value)}}/>
+            </div>
         </div>
     )
 }
