0ad17cbb3e762b229eca8233a913757fc46474ac
thiago <thiagosoares111@yahoo.com.br>
2022-04-24 19:00:00 -0300

    exercicios do useContext
      Dessa vez optei por uma abordagem mais encapsulada para que o contexto geral da aplicação não corresse o risco de ser destruído ou erroneamente alterado.
      Para usarmos o useContext precisamos criar um contexto global com a ajuda do React.createContext que possibilita a criação  de contexto global que pode ser qualquer tipo de dado string, number, object .
      Com o contexto criado, criamos um componente Store e dentro dele colocamos o componente com contexto,  para utiliza o método Provider (<AppContext.Provider>), esse componente contexto deve ter uma propriedade value que passara os valores do contexto e método de altera-los ( aqui usamos o useState para cria um state dentro do componente Store, que guarda o contexto na const state e recebe o método de alteração setState, o qual usaremos depois para construir os métodos de alteração dos valores  únicos como o number e text do contexto).
       Para habilitar o context em toda a aplicação temos que encapsular a "div" dentro do componente mais alto na hierarquia ( no caso App )  com Store.
       Com o Store criado para usar o contexto em qualquer componente usamos o useContext que recebe como parâmetro o AppContext  e do lado esquerdo entre bigodes os valores e metodos dentro do contexto)

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -4,16 +4,19 @@ import { BrowserRouter as Router } from  'react-router-dom'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import Store from '../dataContext/Store'
 
 const App = props => {
 
     return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
+        <Store>
+            <div className="App">
+                <Router>
+                    <Menu />
+                    <Content />
+                </Router>
+            </div>
+        </Store>
     )
 }
 
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { AppContext } from '../../dataContext/Store'
 
 const UseContext = (props) => {
+    const {number,setNumber,text} = useContext(AppContext)
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <SectionTitle title='exercicio 1'/>
+            <div className="center">
+                <span className="text">{number}</span>
+                <span className="text">{text}</span>
+            </div>
+            <div className="center">
+                <button className="btn" onClick={_=>setNumber(number - 1)}>-1</button>
+                <button className="btn" onClick={_=>setNumber(number + 1)}>+1</button>
+            </div>
+
         </div>
     )
 }
